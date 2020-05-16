5c19175b7c0647f7bec2035ff34254e49062b759
Boniface Mwangi <45118623+Nyambura254@users.noreply.github.com>
2020-05-16 22:52:27 +0300

    initial commits

---
+++ chunks.js
@@ -0,0 +1,20 @@
+// --- Directions
+// Given an array and chunk size, divide the array into many subarrays
+// where each subarray is of length size
+// --- Examples
+// chunk([1, 2, 3, 4], 2) --> [[ 1, 2], [3, 4]]
+// chunk([1, 2, 3, 4, 5], 2) --> [[ 1, 2], [3, 4], [5]]
+// chunk([1, 2, 3, 4, 5, 6, 7, 8], 3) --> [[ 1, 2, 3], [4, 5, 6], [7, 8]]
+// chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
+// chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
+
+
+function chunk(array, size) //declare a function 
+{
+    if (!array) return [];
+    const firstChunk = array.slice(0, size); // create the first chunk of the given array
+    if (!firstChunk.length) {
+        return array; // this is the base case to terminate the recursive
+    }
+    return [firstChunk].concat(chunk(array.slice(size, array.length), size));
+}
