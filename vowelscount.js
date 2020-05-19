1e10ad06b5954a755021a4fa5c7e26f79f83c868
Boniface Mwangi <45118623+Nyambura254@users.noreply.github.com>
2020-05-19 13:46:58 +0300

    vowels in a given string

---
+++ vowelscount.js
@@ -0,0 +1,9 @@
+function vowels(str) {
+    var matches = str.match(/[aeiou]/gi);
+    return matches ? matches.length : 0;
+}
+
+function vowels(str) {
+    let count = 0;
+    const checker = ['a', 'e', 'i']
+}
