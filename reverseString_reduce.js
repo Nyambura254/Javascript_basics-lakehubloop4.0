ea62591931f8f66f08a985b4ef97bce3beae9f11
Boniface Mwangi <45118623+Nyambura254@users.noreply.github.com>
2020-05-19 13:49:29 +0300

    initial commit

---
+++ reverseString_reduce.js
@@ -0,0 +1,10 @@
+function reverse(str) {
+    return str.split("").reduce((acc, currentvalue) => acc + currentvalue);
+
+
+}
+var data = [1, 2, 3]
+var result = data.reduce((acc, currentvalue) => acc + currentvalue, 0);
+console
+var data2 = ["a", "b", "c"];
+var result2 = data2.reduce((acc, currentvalue) => acc + currentvalue);
