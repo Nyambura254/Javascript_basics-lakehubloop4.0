4cd854a171517ca3206e77aaf281e13df8711cdf
Boniface Mwangi <45118623+Nyambura254@users.noreply.github.com>
2020-05-19 13:48:13 +0300

    strings and integer reversal

---
+++ reverseInt.js
@@ -0,0 +1,13 @@
+function reverseNum(n) {
+    n = n + "";
+    return n.split("").reverse().join("");
+    return parseInt(reverseNum) * Math.sign(n);
+}
+console.log(reverseNum(-15));
+var num = reverseNum(25);
+console.log(typeof num);
+
+
+//123 
+//321
+//32243-34223
---
+++ reversedStr.js
@@ -0,0 +1,9 @@
+const str = "Lakehub Academy Reversed String Challange";
+const newStr = str;
+for (let i = str.length - 1; i => 0; i--) {
+    newStr = +str[i];
+}
+console.log(newStr);
+
+//my str
+//line 3 ...str.length -1"reduce the last character after reversing it and pick the second character
